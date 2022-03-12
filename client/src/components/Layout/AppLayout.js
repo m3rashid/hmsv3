76d7def2d20df0d9d1a7d3ec54a6346a8f2da389
Sarfraz-droid <Sarf.alam0206>
2022-03-12 23:49:59 +0530

    Navbar

--- client/src/components/Layout/AppLayout.js
+++ client/src/components/Layout/AppLayout.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Layout,
   Typography,
-  // Row,
   Col,
   Button,
+  Menu
 } from "antd";
+import AuthModal from "../Modal/AuthModal";
 import styles from "./layout.module.less";
+
 
 /**
  *
@@ -14,6 +16,46 @@ import styles from "./layout.module.less";
  * @returns {JSX.Element}
  */
 function Index(props) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const MenuRoutes = [
+    {
+      text: "Receptionists",
+      path: "/receptionists"
+    },
+    {
+      text: "Doctors",
+      path: "/doctors"
+    },
+    {
+      text: "Patients",
+      path: "/patients"
+    },
+    {
+      text: "Appointments",
+      path: "/appointments"
+    },
+    {
+      isGroup: true,
+      text: "Admin",
+      path: "/admin/",
+      children: [
+        {
+          text: "Receptionists",
+          path: "/receptionists"
+        },
+        {
+          text: "Doctors",
+          path: "/doctors"
+        },
+        {
+          text: "Patients",
+          path: "/patients"
+        }
+      ]
+    }
+  ]
+
   return (
     <React.Fragment>
       <Layout className={styles.layout}>
@@ -24,18 +66,59 @@ function Index(props) {
                 Dr. M.A Ansari Hospital
               </Typography.Title>
             </Col>
-            <Col span={6}>
-              <Button>Login</Button>
+            <Col span={3}
+              className={styles.theme}
+            >
+              <Button onClick={() => setIsModalVisible(true)} >Login</Button>
+              <AuthModal
+                isModalVisible={isModalVisible}
+                handleOk={() => {
+                  setIsModalVisible(true);
+                }}
+
+                handleCancel={() => {
+                  setIsModalVisible(false);
+                }}
+              />
             </Col>
           </Col>
         </Layout.Header>
         <Layout>
-          <Layout.Sider>Sider</Layout.Sider>
+          <Layout.Sider>
+            <Menu
+              theme="dark"
+              mode="inline"
+            >
+              {MenuRoutes.map((route, index) => {
+
+                if (route.isGroup) {
+                  return (
+                    <Menu.SubMenu key={index} title={route.text}>
+                      {route.children.map((child, i) => {
+                        return (
+                          <Menu.Item key={`${index}_${i}`}>
+                            {child.text}
+                          </Menu.Item>
+                        )
+                      })}
+                    </Menu.SubMenu>
+                  )
+                }
+
+                return (
+                  <Menu.Item key={`${index}`}>
+                    {route.text}
+                  </Menu.Item>
+                )
+              })}
+            </Menu>
+          </Layout.Sider>
           <Layout.Content className={styles.content}>
             {props.children}
           </Layout.Content>
         </Layout>
       </Layout>
+
     </React.Fragment>
   );
 }
