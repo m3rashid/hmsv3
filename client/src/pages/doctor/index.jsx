4b08f45e0a1b840620182abc7b4f7dfe5e510199
Sarfraz <alamsarfraz422@gmail.com>
2022-03-31 03:03:29 +0530

    Doctors module

--- client/src/pages/doctor/index.jsx
+++ client/src/pages/doctor/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Header from "../../components/Header";
+import { Tabs, Divider, Table } from "antd";
+import Appointments from "./modules/Appointments";
 
 const Doctor = () => {
   const [online, setOnline] = React.useState(true);
@@ -8,9 +10,32 @@ const Doctor = () => {
     email: "doctordan@gmail.com",
     online: online,
   };
+
   return (
     <>
-      <Header title="Home" subTitle="" user={user} />;
+      <div
+        style={{
+          padding: "20px",
+        }}
+      >
+        <Header title="Home" subTitle="" user={user} />
+        <Divider />
+
+        <Tabs defaultActiveKey="1">
+          <Tabs.TabPane tab="Appointments" key="1">
+            <Appointments />
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Prescriptions" key="2">
+            Prescriptions Module
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Patients" key="3">
+            Patients Module
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Notifications" key="4">
+            Notifications Module
+          </Tabs.TabPane>
+        </Tabs>
+      </div>
     </>
   );
 };
