886163193a0d2345cae43b4ae6bb7220d36fce71
anzal1 <cofclan145@gmail.com>
2022-05-15 00:54:28 +0530

    anzal

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 
 import { login, logout, revalidate, signup } from "../controllers/auth.js";
+import { createUser } from "../controllers/createUser.js";
 
 router.post("/login", login);
 
@@ -10,5 +11,7 @@ router.post("/signup", signup);
 router.post("/logout", logout);
 
 router.post("/revalidate", revalidate);
+
+router.post("/createUser", createUser);
 
 export default router;
