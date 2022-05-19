0f9e7438b203cd28a2813b8ab58f8321f796b1ef
MOHAMMAD SARFRAZ ALAM <73013838+Sarfraz-droid@users.noreply.github.com>
2022-05-19 17:59:54 +0530

    backend SocketIO fix (#44)

--- server/index.js
+++ server/index.js
@@ -10,12 +10,15 @@ import AuthRoutes from "./routes/auth.routes.js";
 import DoctorRoutes from "./routes/doctor.routes.js";
 import socketHandler from "./routes/sockets/index.js";
 import PatientRoutes from "./routes/patient.routes.js";
+import { instrument } from "@socket.io/admin-ui";
 
 // TODO add a production client here after deployment
-const corsOrigin =
+const corsOrigin = [
   process.env.NODE_ENV === "production"
     ? "http://localhost:3000"
-    : "http://localhost:3000";
+    : "http://localhost:3000",
+  "https://admin.socket.io",
+];
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -42,11 +45,17 @@ if (process.env.NODE_ENV !== "production") {
 }
 const startServer = async () => {
   try {
+    instrument(io, {
+      auth: false,
+    });
+
     await models.sequelize.authenticate({
       logging: process.env.NODE_ENV !== "production",
     });
     console.log("Connection has been established successfully");
-    app.listen(PORT, () => console.log(`Server on http://localhost:${PORT}`));
+    server.listen(PORT, () =>
+      console.log(`Server on http://localhost:${PORT}`)
+    );
   } catch (err) {
     console.log(err);
     process.exit(1);
