8558281ee2afdc17b54caaeccce7878728431f44
m3rashid <mdrashid.hussain.786.01@gmail.com>
2022-05-19 10:32:26 +0530

    somethign

--- server/index.js
+++ server/index.js
@@ -1,22 +1,16 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-<<<<<<< HEAD
-import db from "./models/index.js";
-// import http from "http";
-=======
+// import db from "./models/index.js";
 import http from "http";
 import { Server } from "socket.io";
 
 import models from "./models/index.js";
-import AuthRoutes from "./routes/auth.js";
-import DoctorRoutes from "./routes/doctor.js";
-import socketHandler from "./routes/index.js";
->>>>>>> 1101895 (doctors init)
-
-// import socketHandler from "./routes/index.js";
 import AuthRoutes from "./routes/auth.routes.js";
+import DoctorRoutes from "./routes/doctor.routes.js";
+// import socketHandler from "./routes/index.js";
 import PatientRoutes from "./routes/patient.routes.js";
+
 // TODO add a production client here after deployment
 const corsOrigin =
   process.env.NODE_ENV === "production"
@@ -25,15 +19,15 @@ const corsOrigin =
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: corsOrigin,
-//     methods: ["GET", "POST"],
-//     credentials: true,
-//   },
-// });
-// io.on("connection", (socket) => socketHandler(io, socket));
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: corsOrigin,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+io.on("connection", (socket) => socketHandler(io, socket));
 
 app.use(cors({ origin: corsOrigin, optionsSuccessStatus: 200 }));
 app.use(express.json());
