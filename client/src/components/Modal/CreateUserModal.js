41912382ebf932a1ede365d5559136361d4cbd16
anzal1 <cofclan145@gmail.com>
2022-05-19 13:53:14 +0530

    create user interface

---
+++ client/src/components/Modal/CreateUserModal.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { Modal, Button, Input, Form, message } from "antd";
+import { instance } from "../../api/instance";
+
+function CreateUserModal(props) {
+  const RoleName = props.role[0] + props.role.toLowerCase().slice(1);
+  console.log(RoleName);
+  const onFinish = async (values) => {
+    try {
+      message.loading({
+        content: "Loading...",
+        key: "auth/createUser",
+      });
+
+      const createdUser = {
+        ...values,
+        role: `${props.role}`,
+      };
+
+      console.log(createdUser);
+      const { data } = await instance.post("/auth/createUser", createdUser);
+
+      message.success({
+        content: `${RoleName} created Successfully`,
+        key: "auth/createUser",
+      });
+      props.handleCancel();
+    } catch (error) {
+      message.error({
+        content: `${RoleName} creation Failed`,
+        key: "auth/createUser",
+      });
+    }
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+    message.error({
+      content: `${RoleName} Creation Failed`,
+      key: "auth/createUser",
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <Modal
+        title="Create"
+        visible={props.isModalVisible}
+        onOk={props.handleOk}
+        onCancel={props.handleCancel}
+      >
+        <Form
+          name="Create User"
+          onFinish={onFinish}
+          initialValues={{ remember: true }}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            rules={[{ required: true, message: "Please enter a username!" }]}
+            name="name"
+            label="Name"
+          >
+            <Input placeholder="Name" />
+          </Form.Item>
+          <Form.Item
+            rules={[{ required: true, message: "Please enter an email!" }]}
+            name="email"
+            label="Email"
+          >
+            <Input placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[{ required: true, message: "Please enter your password!" }]}
+          >
+            <Input placeholder="Password" type="password" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Create User
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </React.Fragment>
+  );
+}
+
+export default CreateUserModal;
