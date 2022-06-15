3c92aee600c3d63e92febe657b9390c9a8bef84c
MD Rashid Hussain <64685317+m3rashid@users.noreply.github.com>
2022-06-15 15:00:10 +0530

    add pharmacy and receptionist models (#56)

---
+++ server/services/inventory.js
@@ -0,0 +1,16 @@
+// import db from "../models/index.js";
+// import bcrypt from "bcrypt";
+
+export const addMedicine = async (name, quantity, price, description) => {};
+
+export const removeMedicine = async (medicineId) => {};
+
+export const editMedicine = async (
+  medicineId,
+  name,
+  quantity,
+  price,
+  description
+) => {};
+
+export const getMedicine = async (medicineId) => {};
