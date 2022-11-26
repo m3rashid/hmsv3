4f64b30c0322f581fa80c8d98e95a5c1d8d2ca93
MD Rashid Hussain <m3rashid.hussain@gmail.com>
2022-11-27 01:41:39 +0530

    feature(init*): data migration

--- server/controllers/dataMigration.js
+++ server/controllers/dataMigration.js
@@ -27,10 +27,16 @@ const handleDataMigration = async (req, res) => {
   const [employeeSheet, pensionerSheet, familyPensionerSheet, dependentSheet] =
     supportedSheets.map((s) => XLSX.utils.sheet_to_json(wb.Sheets[s]));
 
-  await handleDataMigration("EMPLOYEE", employeeSheet, "EMP ID");
-  await handleDataMigration("PENSIONER", pensionerSheet, "EMP ID");
-  await handleDataMigration("FAMILY_PENSIONER", familyPensionerSheet, "EMP ID");
-  await handleDataMigration("DEPENDENT", dependentSheet, "EMP ID");
+  const p1 = handleDataMigration("EMPLOYEE", employeeSheet, "EMP ID");
+  const p2 = handleDataMigration("PENSIONER", pensionerSheet, "EMP ID");
+  const p3 = handleDataMigration(
+    "FAMILY_PENSIONER",
+    familyPensionerSheet,
+    "EMP ID"
+  );
+  const p4 = handleDataMigration("DEPENDENT", dependentSheet, "EMP ID");
+
+  await Promise.all([p1, p2, p3, p4]);
 
   console.log("Data Got:", {
     employees: employeeSheet.length,
