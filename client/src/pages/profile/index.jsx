47ceecf82ec001e7224a70839719f7ec9a087ba9
anzal1 <cofclan145@gmail.com>
2023-01-20 00:47:36 +0530

    leave

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,16 +1,39 @@
-import { Button, Form, Input } from "antd";
-import React from "react";
+import { Button, Form, Input, message } from "antd";
+import React, { useEffect } from "react";
 
 import ProfileWrapper from "components/Profile/ProfileWrapper";
+import { useRecoilState } from "recoil";
+import { authState } from "atoms/auth";
+import { instance, socket } from "api/instance";
 
 const ProfilePage = () => {
+  const { user } = useRecoilState(authState);
   const [form] = Form.useForm();
+  const [loading, setLoading] = React.useState(false);
 
   const formSubmitHandler = async (values) => {
-    console.log({ values });
+    setLoading(true);
+    //clear form
+    form.resetFields();
+    try {
+      const { data } = await instance.post("/doctor/leave", values);
+      message.success("Leave assigned successfully");
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+    socket.emit("doctor-left", user);
+    console.log(socket);
     // TODO
     // inform admin that the doctor is leaving
   };
+
+  useEffect(() => {
+    return () => {
+      socket.off("doctor-left");
+    };
+  }, []);
 
   return (
     <ProfileWrapper>
