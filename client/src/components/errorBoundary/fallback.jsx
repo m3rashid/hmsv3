b9fb3c1da26fad5fd55ffef009b596357580e207
MD Rashid Hussain <m3rashid.hussain@gmail.com>
2023-01-24 09:18:41 +0530

    add error boundaries

--- client/src/components/errorBoundary/fallback.jsx
+++ client/src/components/errorBoundary/fallback.jsx
@@ -23,8 +23,8 @@ const ErrorHandlerFallback = ({ removeError }) => {
   return (
     <Result
       status="500"
-      title="400"
-      subTitle="Sorry, something went wrong."
+      title="Sorry, something went wrong."
+      subTitle="Report to the Admin or contact Developers"
       style={{ height: "calc(100vh - 94px)", paddingTop: 150 }}
       extra={
         <Button type="primary" onClick={() => navigate("/")}>
